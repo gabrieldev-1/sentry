b19658552469440f64ac66176d2d14852a2cfead
gabrieldev-1 <developergabrieldossantos@gmail.com>
2026-03-15 19:35:42 -0300

    Help method implemented in Main

--- src/main/java/com/sentry/Main.java
+++ src/main/java/com/sentry/Main.java
@@ -1,10 +1,65 @@
 package com.sentry;
 
 import com.sentry.dashboard.Dashboard;
+
+import com.sentry.SystemSnapShot;
+
 import java.io.IOException;
 
 public class Main {
+
+    public static final String mark = """
+            ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⢀⢀⢀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+            ⠀⠀⠀⠀⠀⠀⠀⠀⢀⠀⡴⠰⠞⠿⠛⠁⠓⠖⠲⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+            ⠀⠀⠀⠀⠀⠀⠀⢸⠆⢁⠶⠿⠇⠹⠁⠸⠷⠏⣈⡀⢰⠀⠈⠀⠀⠀⠀⠀⠀⠀
+            ⠀⠀⠀⠀⠀⠀⡁⠴⠛⢀⡀⠀⠀⢀⠀⠀⠀⠀⡀⠀⠀⠂⠄⠀⠀⠀⠀⠀⠀⠀
+            ⠀⠀⠀⠀⠀⠠⠀⢠⣴⣿⠀⠄⠈⠉⠀⠀⢀⠀⢻⡗⠀⠀⠐⠡⣄⡀⠀⠀⠀⠀
+            ⠀⠀⠀⠀⠀⣤⠒⢺⣿⣿⣆⠙⠄⢤⠠⠔⠘⢢⣞⠋⠀⢀⣰⣧⣬⡇⠀⠀⠀⠀
+            ⠀⠀⠀⠀⠈⠪⡅⠲⢿⢽⣿⣿⣶⣶⣦⣶⣿⠇⠴⠋⠍⢉⣹⣿⠿⠀⠀⠀⠀⠀
+            ⠀⠀⠀⠀⠀⠀⠰⠆⠁⠀⢈⠉⠹⣹⠈⠁⠀⠆⢰⢆⢀⣾⣾⠉⠀⠀⠀⠀⠀⠀
+            ⠀⠀⠀⠀⠀⠀⠀⠀⠃⠷⠀⠄⣤⡀⠀⣠⠠⣤⠄⠼⠟⠉⠀⠀⠀⠀⠀⠀⠀⠀
+            ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠉⠁⠈⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+                               Sentry v1.0
+                      Author: gabrieldev-1
+            """;
+
+    /**
+     * Displays help message with usage instructions for the Sentry monitoring application.
+     */
+    public static void helpMessage() {
+        System.out.println(mark + "\n");
+        System.out.println("DESCRIPTION:");
+        System.out.println("    Real-time system monitoring tool that displays CPU, memory, and process information");
+        System.out.println("    in a terminal-based dashboard using the Lanterna library.");
+        System.out.println();
+        System.out.println("USAGE:");
+        System.out.println("    java -jar sentry.jar [OPTIONS]");
+        System.out.println();
+        System.out.println("OPTIONS:");
+        System.out.println("    -h, --help    Show this help message and exit");
+        System.out.println();
+        System.out.println("CONTROLS:");
+        System.out.println("    Ctrl+C        Exit the application");
+        System.out.println();
+        System.out.println("DISPLAYED INFORMATION:");
+        System.out.println("    - CPU: Individual core usage, total CPU usage, and temperature");
+        System.out.println("    - Memory: Total and used memory with percentage");
+        System.out.println("    - System: Uptime, total processes and threads");
+        System.out.println("    - Processes: Top processes by CPU usage with detailed metrics");
+        System.out.println();
+        System.out.println("EXAMPLES:");
+        System.out.println("    java -jar sentry.jar        # Start monitoring");
+        System.out.println("    java -jar sentry.jar -h     # Show this help");
+        System.out.println("    java -jar sentry.jar --help # Show this help");
+    }
+
     public static void main(String[] args) {
+        // Check for help arguments
+        if (args.length > 0 && (args[0].equals("-h") || args[0].equals("--help"))) {
+            helpMessage();
+            System.exit(0);
+        }
+
         Dashboard dashboard = null;
 
         try {
@@ -25,7 +80,7 @@ public class Main {
 
                 } catch (IOException e) {
                     e.printStackTrace();
-                    
+
                 }
             }
         }
