7db84da99d31e049caf1dac5c030c0dc6528b46f
gabrieldev-1 <developergabrieldossantos@gmail.com>
2026-03-15 20:26:45 -0300

    Generating the JAR file for release 1.3 of the program.

--- src/main/java/com/sentry/Main.java
+++ src/main/java/com/sentry/Main.java
@@ -54,7 +54,6 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        // Check for help arguments
         if (args.length > 0 && (args[0].equals("-h") || args[0].equals("--help"))) {
             helpMessage();
             System.exit(0);
